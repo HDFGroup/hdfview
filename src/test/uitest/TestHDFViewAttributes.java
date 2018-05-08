a11a45bcf869f7ff02b9d950f8eea7bc14a173e4
Jordan Henderson <jhenderson@hdfgroup.org>
2018-05-08 13:59:06 -0500

    Add base Attribute test file

---
+++ src/test/uitest/TestHDFViewAttributes.java
@@ -0,0 +1,181 @@
+package test.uitest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestHDFViewAttributes extends AbstractWindowTest {
+    @Test
+    public void testAddAttribute() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testDeleteAttribute() {
+        try {
+            /* Test deletion of attribute by button */
+
+            /* Test deletion of attribute by popup menu */
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testRenameAttribute() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testOpenScalarAttribute() {
+        try {
+            /* Test open of attribute by double-click */
+
+            /* Test open of attribute by popup menu */
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testOpenCompoundAttribute() {
+        try {
+            /* Test open of attribute by double-click */
+
+            /* Test open of attribute by popup menu */
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testEditScalarAttribute() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testEditCompoundAttribute() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testDiscardScalarAttributeEditResults() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testDiscardCompoundAttributeEditResults() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testHDF5RenameAttributeFunctionDisabled() {
+        try {
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+    @Test
+    public void testHDF4DeleteAttributeFunctionDisabled() {
+        try {
+            /*
+             * Test that both the "Delete Attribute" button and popup MenuItem are disabled
+             */
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+        catch (AssertionError ae) {
+            ae.printStackTrace();
+            fail(ae.getMessage());
+        }
+    }
+
+}
