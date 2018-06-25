67901a7c724e3748e2fa38d7fc74568369fdb421
Allen Byrne <byrn@hdfgroup.org>
2018-06-25 10:01:45 -0500

    Remove unsopported checks

--- src/hdf/object/h5/H5CompoundDS.java
+++ src/hdf/object/h5/H5CompoundDS.java
@@ -649,32 +649,6 @@ public class H5CompoundDS extends CompoundDS {
             log.debug("read(): get datatype: ", ex);
         }
 
-        /*
-         * Check for any unsupported datatypes and fail early before
-         * attempting to read the dataset
-         */
-        if (DSdatatype.isArray() || DSdatatype.isVLEN()) {
-            H5Datatype baseType = (H5Datatype) DSdatatype.getDatatypeBase();
-
-            if (baseType != null) {
-                if (DSdatatype.isArray() && baseType.isCompound()) {
-                    log.debug("read(): cannot read dataset of type ARRAY of COMPOUND");
-                    log.trace("read(): finish");
-                    throw new HDF5Exception("Unsupported dataset of type ARRAY of COMPOUND");
-                }
-
-                if (DSdatatype.isVLEN() && baseType.isCompound()) {
-                    log.debug("read(): cannot read dataset of type VLEN of COMPOUND");
-                    log.trace("read(): finish");
-                    throw new HDF5Exception("Unsupported dataset of type VLEN of COMPOUND");
-                }
-            }
-            else {
-                log.debug("read(): ARRAY or VLEN datatype has no base type");
-                throw new Exception("Dataset's datatype (ARRAY or VLEN) has no base datatype");
-            }
-        }
-
         if (isExternal) {
             String pdir = this.getFileFormat().getAbsoluteFile().getParent();
 
@@ -774,24 +748,6 @@ public class H5CompoundDS extends CompoundDS {
                     }
                     catch (Exception ex) {
                         log.debug("read(): get member {} size failure: ", i, ex);
-                        continue;
-                    }
-
-                    /*
-                     * Check for any unsupported datatypes before continuing with
-                     * this compound member
-                     */
-                    if (member_type.isRegRef() || (member_type.isArray() && member_base.isArray())) {
-                        String[] nullValues = new String[(int) totalSelectedSpacePoints];
-                        String errorStr = "*unsupported*";
-
-                        for (int j = 0; j < totalSelectedSpacePoints; j++)
-                            nullValues[j] = errorStr;
-
-                        memberDataList.add(nullValues);
-
-                        log.debug("read(): {} Member[{}] of type {} is unsupported.", member_name, i, member_type.getDescription());
-
                         continue;
                     }
 
