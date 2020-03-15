1cd8ff7651904ce092d4d44a9bb94a947e064886
Allen Byrne <byrn@hdfgroup.org>
2020-03-15 08:48:07 -0500

    Remove obsolete variable

--- src/hdf/HDFVersions.java
+++ src/hdf/HDFVersions.java
@@ -4,5 +4,4 @@ public interface HDFVersions {
     public static final String HDF4_VERSION    = @HDF4_VERSION@;
     public static final String HDF5_VERSION    = @HDF5_VERSION@;
     public static final String HDFVIEW_VERSION = @HDFVIEW_VERSION@;
-    public static final String HDFJAVA_VERSION = @HDFJAVA_VERSION@;
 }
