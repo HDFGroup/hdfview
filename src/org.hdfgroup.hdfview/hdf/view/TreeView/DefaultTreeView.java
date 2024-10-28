260c2b0096b233e771987dc52b18e6bcef3ffadf
Allen Byrne <byrn@hdfgroup.org>
2024-10-28 12:36:17 -0500

    Update to latest develop vesrsion

--- src/org.hdfgroup.hdfview/hdf/view/TreeView/DefaultTreeView.java
+++ src/org.hdfgroup.hdfview/hdf/view/TreeView/DefaultTreeView.java
@@ -3334,15 +3334,15 @@ public class DefaultTreeView implements TreeView {
             openShell.setImages(ViewProperties.getHdfIcons());
             openShell.setLayout(new GridLayout(1, true));
 
-            String[] lowValues  = {"Earliest", "V18", "V110", "V112", "V114", "V116", "Latest"};
+            String[] lowValues  = {"Earliest", "V18", "V110", "V112", "V114", "V200", "Latest"};
             int[] lowConstants  = {HDF5Constants.H5F_LIBVER_EARLIEST, HDF5Constants.H5F_LIBVER_V18,
                                   HDF5Constants.H5F_LIBVER_V110,     HDF5Constants.H5F_LIBVER_V112,
-                                  HDF5Constants.H5F_LIBVER_V114,     HDF5Constants.H5F_LIBVER_V116,
+                                  HDF5Constants.H5F_LIBVER_V114,     HDF5Constants.H5F_LIBVER_V200,
                                   HDF5Constants.H5F_LIBVER_LATEST};
-            String[] highValues = {"V18", "V110", "V112", "V114", "V116", "Latest"};
+            String[] highValues = {"V18", "V110", "V112", "V114", "V200", "Latest"};
             int[] highConstants = {HDF5Constants.H5F_LIBVER_V18,  HDF5Constants.H5F_LIBVER_V110,
                                    HDF5Constants.H5F_LIBVER_V112, HDF5Constants.H5F_LIBVER_V114,
-                                   HDF5Constants.H5F_LIBVER_V116, HDF5Constants.H5F_LIBVER_LATEST};
+                                   HDF5Constants.H5F_LIBVER_V200, HDF5Constants.H5F_LIBVER_LATEST};
 
             // Try to retrieve the existing version bounds
             int[] current = null;
